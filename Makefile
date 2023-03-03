433ff408475ae590243032efe871ed5873a1393a
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2023-03-03 22:10:25 +0100

    Add initial files

---
+++ Makefile
@@ -0,0 +1,26 @@
+build: listings/generated
+	rm -f main.pdf
+	make main.pdf
+	exiftool \
+		-Title="The Conversion of Source Code to Machine Code" \
+		-Author="Silas Groh, Mik Müller" \
+		-Subject="Compiler Construction" \
+		-Keywords="compiler, compiler construction, programming language" \
+		-overwrite_original \
+		main.pdf
+
+main.pdf: main.tex deps preamble lirstings.json
+	latexmk -lualatex -shell-escape -g main.tex
+
+init: fetch_deps.sh
+	sh fetch_deps.sh
+	mkdir -p ./deps/paper/deps
+	cp -r ./deps/rush ./deps/paper/deps/rush
+	cargo install --git https://github.com/rush-rs/lirstings --force
+
+listings/generated:
+	cd deps/paper && make listings/generated && cd ..
+
+clean:
+	eztex c
+	rm -f lirstings.cache.json
---
+++ fetch_deps.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# This file is used to download used git dependencies like tree-sitter parsers or the rush project.
+
+set -e
+
+mkdir -p deps
+
+fetch_repo() {
+    if ! [ -d "deps/$2" ]; then
+        git clone "https://github.com/$1/$2.git" "deps/$2"
+    else
+        cd "deps/$2"
+        git pull
+        cd ../..
+    fi
+}
+
+fetch_repo nvim-treesitter nvim-treesitter
+
+fetch_repo tree-sitter tree-sitter-rust
+fetch_repo tree-sitter tree-sitter-bash
+fetch_repo tree-sitter tree-sitter-c
+fetch_repo RubixDev ebnf
+# fetch_repo benwilliamgraham tree-sitter-llvm
+fetch_repo RubixDev tree-sitter-llvm
+fetch_repo rush-rs tree-sitter-rush
+fetch_repo rush-rs tree-sitter-asm
+fetch_repo rush-rs tree-sitter-hexdump
+fetch_repo wasm-lsp tree-sitter-wasm
+fetch_repo rush-rs tree-sitter-wasm-queries
+
+fetch_repo rush-rs rush
+fetch_repo rush-rs paper
